10c8662f3ef0ceb78864c1dfcd649e42b30e0aed
Max Moreland <52013003+mmoreland@users.noreply.github.com>
2020-03-02 09:23:51 -0700

    Update copy.cpp

--- copy.cpp
+++ copy.cpp
@@ -1,7 +1,16 @@
 #include <fstream>
-
+#include <iostream>
 int main(){
-    std::ifstream  src("test.txt", std::ios::binary);
-    std::ofstream  dst("next.txt",   std::ios::binary);
-    dst << src.rdbuf();
+  std::string file;
+  std::string nFile;
+
+  std::cout << "Enter the file name: ";
+  std::cin >> file;
+
+  std::cout << "Enter the new file name: ";
+  std::cin >> nFile;
+
+  std::ifstream  src(file, std::ios::binary);
+  std::ofstream  dst(nFile,   std::ios::binary);
+  dst << src.rdbuf();
 }
